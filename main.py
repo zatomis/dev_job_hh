7d4232feeffa8b28e67239f749b68a0b190adb9f
zatomis <zatomis@list.ru>
2023-08-09 16:37:10 +0300

    Оптимизация кода

--- main.py
+++ main.py
@@ -10,18 +10,15 @@ POPULAR_LANGUAGES = ['JavaScript', 'Java', 'Python', 'Ruby', 'PHP', 'C++', 'CSS'
 
 
 def predict_rub_salary(vacancy: dict):
-    if vacancy:
-        if vacancy['currency'] != 'RUR':
-            return None
-        if vacancy['from'] and vacancy['to']:
-            return int((vacancy['from'] + vacancy['to']) / 2)
-        if not vacancy['from']:
-            return int(vacancy['to'] * 0.8)
-        if not vacancy['to']:
-            return int(vacancy['from'] * 1.2)
-        if not vacancy['from'] and not vacancy['to']:
-            return None
-    else:
+    if vacancy['currency'] != 'RUR':
+        return None
+    if vacancy['from'] and vacancy['to']:
+        return int((vacancy['from'] + vacancy['to']) / 2)
+    if not vacancy['from']:
+        return int(vacancy['to'] * 0.8)
+    if not vacancy['to']:
+        return int(vacancy['from'] * 1.2)
+    if not vacancy['from'] and not vacancy['to']:
         return None
 
 
@@ -36,22 +33,22 @@ def get_salary_statistics_hh(location):
     for language in POPULAR_LANGUAGES:
         page = 0
         payload = {
-                    'text': f"Программист {language}",
-                    'area': location,
-                    'date_published_from': unix_time,
-                    'page': page
-                    }
+            'text': f"Программист {language}",
+            'area': location,
+            'date_published_from': unix_time,
+            'page': page
+        }
         vacancies_processed = 0
         amount_of_salaries = 0
         total_pages = 1
         while page < total_pages:
             response = requests.get(url, payload)
             response.raise_for_status()
-            job_list = response.json()
+            work_options = response.json()
             page = payload['page']
-            vacancies_found = job_list['found']
-            jobs = job_list['items']
-            total_pages = job_list['pages']
+            vacancies_found = work_options['found']
+            jobs = work_options['items']
+            total_pages = work_options['pages']
             for job in jobs:
                 salary = predict_rub_salary(job['salary'])
                 if salary:
@@ -61,11 +58,17 @@ def get_salary_statistics_hh(location):
             payload['page'] = page
         if vacancies_processed:
             average_salary = int(amount_of_salaries/vacancies_processed)
-        statistics = {
-            'vacancies_found': vacancies_found,
-            'vacancies_processed': vacancies_processed,
-            'average_salary': average_salary,
-        }
+            statistics = {
+                'vacancies_found': vacancies_found,
+                'vacancies_processed': vacancies_processed,
+                'average_salary': average_salary,
+            }
+        else:
+            statistics = {
+                'vacancies_found': 0,
+                'vacancies_processed': 0,
+                'average_salary': 0,
+            }
         wage[language] = statistics
     return wage
 
@@ -96,10 +99,10 @@ def get_salary_statistics_sj(location, api_key):
             total_vacancies = response.json()['total']
             for job in work_options['objects']:
                 job_salary = {
-                                'currency': str(job['currency']).upper().replace('RUB', 'RUR'),
-                                'from': job['payment_from'],
-                                'to': job['payment_to']
-                                }
+                    'currency': str(job['currency']).upper().replace('RUB', 'RUR'),
+                    'from': job['payment_from'],
+                    'to': job['payment_to']
+                }
                 salary_js = predict_rub_salary(job_salary)
                 if salary_js:
                     amount_of_salaries = amount_of_salaries + int(salary_js)
@@ -109,11 +112,17 @@ def get_salary_statistics_sj(location, api_key):
             payload['page'] = page + 1
         if vacancies_processed:
             average_salary = int(amount_of_salaries/vacancies_processed)
-        statistics = {
-            'vacancies_found': total_vacancies,
-            'vacancies_processed': vacancies_processed,
-            'average_salary': average_salary,
-        }
+            statistics = {
+                'vacancies_found': total_vacancies,
+                'vacancies_processed': vacancies_processed,
+                'average_salary': average_salary,
+            }
+        else:
+            statistics = {
+                'vacancies_found': 0,
+                'vacancies_processed': 0,
+                'average_salary': 0,
+            }
         wage[language] = statistics
     return wage
 
